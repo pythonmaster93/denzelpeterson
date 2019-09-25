80742173c89458f634624dee0fd914d40b4a066f
pythonmaster93 <54456994+pythonmaster93@users.noreply.github.com>
2019-09-24 23:15:55 -0400

    Add files via upload

---
+++ P1M2DenzelPeterson.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #  Module 2 Required Coding Activity  
+# Introduction to Python Unit 1  
+# 
+# This is an activity based on code similar to the Jupyter Notebook **`Practice_MOD02_1-3_IntroPy.ipynb`** which you may have completed.
+# 
+# | Some Assignment Requirements |  
+# |:-------------------------------|  
+# | **NOTE:** This program requires a **function** be defined, created and called. The call will send values based on user input.  The function call must capture a `return` value that is used in print output.  The function will have parameters and `return` a string and should otherwise use code syntax covered in module 2.  |  
+#  
+# ## Program: fishstore()
+# create and test fishstore()
+# - **fishstore() takes 2 string arguments: fish & price**
+# - **fishstore returns a string in sentence form**  
+# - **gather input for fish_entry and price_entry to use in calling fishstore()**
+# - **print the return value of fishstore()**
+# >example of output: **`Fish Type: Guppy costs $1`**
+
+# In[2]:
+
+
+# [ ] create, call and test fishstore() function 
+# then PASTE THIS CODE into edX
+
+def fishstore(fish, price):
+    print('Fish Type: ' + fish + ', costs $' + price + '.')
+fish_entry = input("enter the fish type: ")
+price_entry = input("enter the price: ")
+fishstore(fish_entry, price_entry)
+
+
+# ### Need assignment tips and clarification? 
+# See the video on the "End of Module coding assignment > Module 2 Required Code Description" course page on [edX](https://courses.edx.org/courses/course-v1:Microsoft+DEV236x+4T2017/course)    
+# 
+# # Important:  [How to submit the code in edX by pasting](https://courses.edx.org/courses/course-v1:Microsoft+DEV236x+1T2017/wiki/Microsoft.DEV236x.3T2018/paste-code-end-module-coding-assignments/)
+
+# [Terms of use](http://go.microsoft.com/fwlink/?LinkID=206977) &nbsp; [Privacy & cookies](https://go.microsoft.com/fwlink/?LinkId=521839) &nbsp; © 2017 Microsoft
